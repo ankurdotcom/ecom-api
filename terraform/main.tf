dd0ec41a8e7c0ebd3505afa22a8e2a53b0301dd4
Ankur Gupta <ankurdotcom@gmail.com>
2024-12-16 13:00:25 +0530

    redis setup done using terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,26 +14,6 @@ provider "google" {
   credentials = "keys.json"
 }
 
-# Pub/Sub Module
-module "pubsub" {
-  source = "./modules/pubsub"
-
-  topics = ["inventory-events", "payment-events", "order-updates"]
-
-  subscriptions = [
-    {
-      topic_name           = "inventory-events"
-      subscription_name    = "inventory-sub"
-      ack_deadline_seconds = 20
-    },
-    {
-      topic_name           = "payment-events"
-      subscription_name    = "payment-sub"
-      ack_deadline_seconds = 10
-    }
-  ]
-}
-
 # Redis Module
 module "redis" {
   source        = "./modules/redis"
@@ -43,28 +23,6 @@ module "redis" {
   tier          = "BASIC"
 }
 
-# Cloud SQL Module
-module "cloud_sql" {
-  source        = "./modules/cloud_sql"
-  instance_name = "ecom-db"
-  database_name = "ecommerce"
-  user          = "admin"
-  password      = "admin123"
-  region        = "us-central1"
-}
-
-output "pubsub_topics" {
-  value = module.pubsub.topic_names
-}
-
-output "pubsub_subscriptions" {
-  value = module.pubsub.subscription_names
-}
-
 output "redis_instance" {
   value = module.redis.redis_instance_name
-}
-
-output "cloud_sql_connection" {
-  value = module.cloud_sql.cloud_sql_connection_name
 }
